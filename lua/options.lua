e7cdf38f08cbaa19adace33ddd4a5a6fa51ee2d5
Jasper Concepcion <jasper@manok.dev>
2024-11-16 16:41:28 +0800

    chore: add personal config

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,24 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+o.relativenumber = true
+o.scrolloff = 10
+vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
+o.colorcolumn = "80,100"
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
+  end,
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
---
+++ lua/plugins/utils.lua
@@ -0,0 +1,44 @@
+-- NOTE:
+-- HACK:
+-- FIXME:
+-- BUG:
+-- ISSUE:
+-- FIXIT:
+-- PERF:
+-- TODO:
+-- WARN:
+-- TEST:
+
+return {
+  { "mg979/vim-visual-multi", lazy = false }, -- Visual multi cursor
+  { "numToStr/Comment.nvim", opts = {}, lazy = false },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("nvim-autopairs").setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      -- local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      -- local cmp = require("cmp")
+      -- cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = true },
+  },
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim", -- optional
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {}, -- your configuration
+  },
+}
